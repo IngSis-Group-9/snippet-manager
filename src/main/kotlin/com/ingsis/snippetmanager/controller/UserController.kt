b71dd7bfd39326a4548152a5b6a1b9b6311ec251
pedrojschmidt <pedro.schmidt@ing.austral.edu.ar>
2024-07-02 12:37:02 -0300

    Register user only with auth token

--- src/main/kotlin/com/ingsis/snippetmanager/controller/UserController.kt
+++ src/main/kotlin/com/ingsis/snippetmanager/controller/UserController.kt
@@ -4,11 +4,12 @@ import com.ingsis.snippetmanager.model.de.User
 import com.ingsis.snippetmanager.service.UserService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -19,9 +20,12 @@ import org.springframework.web.bind.annotation.RestController
 class UserController(private val userService: UserService) {
     @PostMapping("/register")
     fun register(
-        @RequestBody userRegister: UserRegister,
+        @AuthenticationPrincipal jwt: Jwt,
     ): ResponseEntity<User> {
-        val newUser = userService.registerUser(userRegister.getId(), userRegister.getName(), userRegister.getEmail())
+        val userId = jwt.subject
+        val name = jwt.getClaimAsString("https://ingisis-group-9/name")
+        val email = jwt.getClaimAsString("https://ingisis-group-9/email")
+        val newUser = userService.registerUser(userId, name, email)
         return ResponseEntity.ok(newUser)
     }
 
